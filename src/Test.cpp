55f4dedd59010c6c79fc32f61ac0d66e53e3b06b
Артур <cathykri@student.21-school.ru>
2024-12-11 03:00:03 +0700

    all done

--- src/Test.cpp
+++ src/Test.cpp
@@ -2,21 +2,25 @@
 #include <catch2/catch.hpp>
 #include "MyClient.cpp"
 
-TEST_CASE("Authorization test") {
-MyClient client;
-SECTION("Authorization with wrong credentials should return null") {
-    auto wr_ptr = client.Authorization("admin_wrong", "123");
-    REQUIRE(wr_ptr == nullptr);
+TEST_CASE("Authorization test")
+{
+    MyClient client;
+    SECTION("Authorization with wrong credentials should return null")
+    {
+        auto wr_ptr = client.Authorization("admin_wrong", "123");
+        REQUIRE(wr_ptr == nullptr);
+    }
+
+    SECTION("Authorization with correct credentials should return pointer")
+    {
+        auto ptr = client.Authorization("admin", "123");
+        REQUIRE(ptr != nullptr);
+        delete ptr;
+    }
 }
 
-SECTION("Authorization with correct credentials should return pointer") {
-    auto ptr = client.Authorization("admin", "123");
-    REQUIRE(ptr != nullptr);
-    delete ptr;
-}
-}
-
-TEST_CASE("test_registration") {
+TEST_CASE("test_registration")
+{
     MyClient client;
     auto ptr = client.Registration("admin", "123", Roles::AdminRole);
     REQUIRE(ptr == nullptr);
@@ -25,21 +29,24 @@ TEST_CASE("test_registration") {
     delete ptr;
 }
 
-TEST_CASE("test_delete") {
+TEST_CASE("test_delete")
+{
     MyClient client;
     auto ptr = client.Delete("", "user");
     REQUIRE(ptr != nullptr);
     delete ptr;
 }
 
-TEST_CASE("test_change_user_data") {
+TEST_CASE("test_change_user_data")
+{
     MyClient client;
     auto ptr = client.ChangeUserData("", "user", "123", "1234");
     REQUIRE(ptr != nullptr);
     delete ptr;
 }
 
-TEST_CASE("test_all_users") {
+TEST_CASE("test_all_users")
+{
     MyClient client;
     auto ptr = client.AllUsers("");
     REQUIRE(ptr != nullptr);
